75f7c0ff0cad701b2cbdd2bacca4a5e4044b8b11
jose cristiano Ronaldo Medina <joseparakefla13@gmail.com>
2026-05-11 21:37:53 -0600

    se implemento mejoras en clase pedido y nueva forma de la clase inevntario

---
+++ InventoryItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proyecto_Asados_La_Flaca_Versión_1._1_Completo
+{
+    public class InventoryItem
+    {
+        public string Name { get; set; }    // Nombre del insumo
+        public int Quantity { get; set; }   // Cantidad disponible
+        public int MinStock { get; set; }   // Stock mínimo
+
+        public void Use(int amount)
+        {
+            if (amount > 0 && amount <= Quantity)
+                Quantity -= amount;
+        }
+
+        public bool IsLowStock()
+        {
+            return Quantity <= MinStock;
+        }
+    }
+}
+
+
---
+++ Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace Proyecto_Asados_La_Flaca_Versión_1._1_Completo
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<DishItem> Dishes { get; set; }
+
+        public Order()
+        {
+            Dishes = new List<DishItem>();
+            CreatedAt = DateTime.Now;
+        }
+
+        // Agregar un plato al pedido
+        public void AddDish(DishItem dish)
+        {
+            Dishes.Add(dish);
+        }
+
+        // Completar pedido: descuenta platos e insumos del inventario
+        public void CompleteOrder(Inventory inventory)
+        {
+            foreach (var dish in Dishes)
+            {
+                dish.ConsumeDish(inventory);
+            }
+        }
+
+    }
+
+}
+
+
+
+
+
